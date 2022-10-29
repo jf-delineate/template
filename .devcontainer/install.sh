c45970af4e8d72ac34f99b8396bde752f25dc3c1
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2022-10-29 14:32:44 +0000

    Install Kubectl, include Terraform Linux provider hashes, markdown link config formatting

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -8,3 +8,9 @@ pip install -r .devcontainer/requirements.txt
 
 # go installs ----------------------------------------------------
 go install github.com/charmbracelet/glow@latest
+
+# kubectl installs -----------------------------------------------
+curl -L -o /tmp/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -L -o /tmp/kubectl.sha256 "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat /tmp/kubectl.sha256)  /tmp/kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 /tmp/kubectl /usr/local/bin/kubectl
